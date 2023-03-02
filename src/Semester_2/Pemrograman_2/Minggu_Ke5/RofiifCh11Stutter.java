6a4ceb6d768b52f2e53e6f4fdc9e6d576c233df2
RofiifSyarof <rofiifsyarof@gmail.com>
2023-03-02 11:51:40 +0700

    Upload Latihan Stutter

---
+++ src/Semester_2/Pemrograman_2/Minggu_Ke5/RofiifCh11Stutter.java
@@ -0,0 +1,31 @@
+package Semester_2.Pemrograman_2.Minggu_Ke5;
+
+/*
+ * Muhammad Rofiif Syarof Nur Aufaa
+ * 22537141014
+ */
+
+import java.util.ArrayList;
+public class RofiifCh11Stutter {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<Integer>(); 
+        myList.add(1);
+        myList.add(8);
+        myList.add(19);
+        myList.add(4);
+        myList.add(17);
+        System.out.println("list awal: " + myList);
+        stutter(myList);
+        System.out.println("list stutter: " + myList);
+    }
+    public static void stutter(ArrayList<Integer> myList) {
+        for (int i = 0; i < myList.size(); i += 2) {
+            myList.add(i, myList.get(i));
+        }
+    }
+}
+/*
+Output:
+list awal: [1, 8, 19, 4, 17]
+list stutter: [1, 1, 8, 8, 19, 19, 4, 4, 17, 17]
+ */
